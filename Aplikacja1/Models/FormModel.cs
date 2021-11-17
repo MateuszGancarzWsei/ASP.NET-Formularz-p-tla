99146b85a1058cb4a210dfb532540b5a8c03e631
MateuszGancarzWsei <93799998+MateuszGancarzWsei@users.noreply.github.com>
2021-11-18 00:05:46 +0100

    Add files via upload

---
+++ Aplikacja1/Models/FormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aplikacja1.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Imię")]
+        public string Name { get; set; }
+
+        [Display(Name = "Liczba powtórzeń")]
+        public int NumOfReplications { get; set; }
+        public int Replications { get; set; }
+    }
+}
